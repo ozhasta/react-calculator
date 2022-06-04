a95583e6ae77b6e9e1ac78d5f67346038719f47b
Turgut Sezer <inv@l.id>
2022-06-04 04:39:58 +0300

    improve code quality

--- src/App.js
+++ src/App.js
@@ -5,75 +5,112 @@ import Buttons from "./Buttons"
 const defaultState = {
   currentInput: "0",
   previousInput: "",
-  operation: ""
+  operation: "",
+  invalidOperation: false
 }
+
+const invalidOperationState = {
+  previousInput: "",
+  currentInput: "Invalid operation",
+  operation: "",
+  invalidOperation: true
+}
+
 function reducer(state, action) {
   action.type = action.type === "*" ? "x" : action.type
   action.type = action.type === "/" ? "÷" : action.type
+
+  // first and second numeric inputs are valid (previousInput/num1, currentInput/num2),
+  // operation input chosen (+ - * /), ready for calculation
+  const validForCalculation =
+    (state.previousInput || state.previousInput === "0") &&
+    (state.currentInput || state.currentInput === "0") &&
+    state.operation
+
+  // previous numeric input present, operation input chosen
+  // ready for chaining operations based on previous result
+  const validForChainOperation = state.previousInput && !state.currentInput && state.operation
 
   switch (action.type) {
     case "x":
     case "÷":
     case "-":
     case "+":
+      if (validForCalculation) {
+        const result = calculate(state)
+        isNaN(result)
+          ? invalidOperationState
+          : {
+              ...state,
+              currentInput: "",
+              operation: action.type,
+              previousInput: result
+            }
+      }
+
+      if (validForChainOperation) {
+        return {
+          ...state,
+          operation: action.type
+        }
+      }
       return {
         ...state,
-        previousInput: state.currentInput + action.type,
+        previousInput: state.currentInput,
         currentInput: "",
         operation: action.type
       }
-
+    // TODO: similiar operations
     case "Enter":
     case "=":
-      if (
-        state.previousInput &&
-        state.currentInput &&
-        state.operation &&
-        state.currentInput !== "."
-      ) {
-        const result = compute(state)
-        return {
-          ...state,
-          currentInput: result,
-          previousInput: ""
-        }
-      } else {
-        return state
+      if (validForCalculation) {
+        const result = calculate(state)
+        isNaN(result)
+          ? invalidOperationState
+          : {
+              ...state,
+              currentInput: result,
+              operation: "",
+              previousInput: ""
+            }
       }
+      return state
 
     case "Escape":
     case "Delete":
       return defaultState
 
     case "Backspace":
-      let tempResult = state.currentInput.substring(0, state.currentInput.length - 1)
-      tempResult = state.currentInput.length > 1 ? tempResult : "0"
+      let tempResult = state.currentInput.toString().substring(0, state.currentInput.length - 1)
       return {
         ...state,
-        currentInput: tempResult
+        currentInput: state.currentInput.toString().length > 1 ? tempResult : "0"
       }
 
     case ".":
     case ",":
-      if (!state.currentInput.includes(".")) {
-        return {
-          ...state,
-          currentInput: (state.currentInput += state.currentInput ? "." : "0.")
-        }
-      } else {
+      if (state.currentInput.toString().includes(".")) {
         return state
+      }
+      return {
+        ...state,
+        currentInput: state.currentInput === "" ? "0." : state.currentInput.concat(".")
       }
 
     default:
+      if (isNaN(state.currentInput) || state.currentInput.length >= 12) {
+        return state
+      }
       if (state.currentInput === "0") {
         return { ...state, currentInput: action.type }
-      } else {
-        return { ...state, currentInput: state.currentInput.concat(action.type) }
+      }
+      return {
+        ...state,
+        currentInput: state.currentInput.toString().concat(action.type)
       }
   }
 }
-
-function compute(state) {
+function calculate(state) {
   let result
   let num1 = parseFloat(state.previousInput)
   let num2 = parseFloat(state.currentInput)
@@ -90,9 +127,12 @@ function compute(state) {
     case "÷":
       result = num1 / num2
       break
+    default:
+      throw new Error("Invalid operation")
   }
-  result = parseFloat(result.toFixed(8))
-  return result
+  // decimal length
+  result = parseFloat(result.toFixed(7))
+  return result.toString()
 }
 
 function App() {
@@ -101,6 +141,9 @@ function App() {
   function handleClick(e) {
     e.preventDefault()
     if (e.target.dataset.input !== undefined) {
+      if (state.invalidOperation) {
+        dispatch({ type: "Delete" })
+      }
       dispatch({ type: e.target.dataset.input })
     }
   }
@@ -111,7 +154,7 @@ function App() {
         <div className="screen">
           <div id="previous-screen-container">
             <span id="previous-screen">{state.previousInput}</span>
-            <span id="previous-screen-operand"></span>
+            <span id="previous-screen-operand"> {state.operation}</span>
           </div>
           <div id="current-screen">{state.currentInput}</div>
         </div>
